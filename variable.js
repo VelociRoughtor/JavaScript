acaa9a9b38a08005d8a1d84bfd4aead9f491f85f
YourUsername <mdmehedihasanrafy67865@gmail.com>
2024-08-29 21:21:00 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,17 @@
+/*
+
+Variables in JavaScript
+
+Five things to declare a variable
+
+*/
+
+var weight = 38;
+var money = 30;
+console.log(weight);
+console.log(money);
+money=400;
+console.log(money);
+
+var name = "rafy";
+console.log(name);
