c77fcfaf99cada39c4571634155f1c9d88396b3e
YourUsername <mdmehedihasanrafy67865@gmail.com>
2024-08-30 04:07:42 +0600

    Variables and Constants in JavaScript

---
+++ Get_Started/constant.js
@@ -0,0 +1,30 @@
+
+/*
+***************************************
+*               Constant              *
+***************************************
+
+Declaration and Initialization:
+
+syntax: const constant_name = value;
+
+*/
+
+const pie = 3.1416;
+console.log(pie);
+
+// pie = 7.3;
+// We can't reassign a constant. it will show an Type Error message.
+
+/*
+
+Quick QnA
+
+1. What is the purpose of a constant?
+ans: The purpose of a constant is to define data that can't be changed.
+
+2. what is the difference between a constant and a variable?
+ans: A variable can store data that can be changed and also reassign later.
+A constant can't store data that can be changed and also can't reassign later.
+
+*/
---
+++ Get_Started/variables.js
@@ -0,0 +1,54 @@
+/* 
+***************************************
+*               Variables             *
+***************************************
+
+1. Variable Declaration:
+
+Syntax : var variable_name ;
+
+2. Variable Initialization:
+
+Syntax : variable_name = data;
+
+if data is a string write " " between these. using ' ' is also fine. 
+else if its a number write number(integer or float)
+else its a boolean type data write true/false
+
+example: 
+
+*/
+
+// var firstName;
+// firstName = "Steven";
+
+// if we combine both:
+
+var firstName = "Steven";
+
+// we can write let instead of var 
+
+let lastName = "Han";
+
+// '=' is called assignment operator.
+
+
+// Variable naming rules: Reserved keyword can't be named as a variable
+// We use camelCase Notation for naming variable.
+
+// To print the output we use:
+
+console.log(firstName);
+console.log(lastName);
+
+
+/*
+    Quick QnA
+
+    1. What is a variable?
+    ans: A variable is a named storage for data.
+
+    2. How do you declare a variable?
+    ans: Declare the variable with var or let keyword.
+*/
+
