deb4aa152a4fc2c97a6ebe2adfb00113dabc5be9
YourUsername <mdmehedihasanrafy67865@gmail.com>
2024-09-10 04:46:43 +0600

    array-tasks

---
+++ Programming_Hero/array_tasks.js
@@ -0,0 +1,39 @@
+// Task 1
+// let fruits = ['apple', 'banana', 'coconut', 'date', 'elderberry'];
+// console.log(fruits[3]);
+// fruits[2] = 'jambura';
+// console.log(fruits);
+
+
+// Task 2
+// let touristDestination = ['Saint-Martin', 'Tanguar-Haor', 'Kuakata-Sea-Beach'];
+// touristDestination.push('Sundarban');
+// touristDestination.push('Sonargaon', 'Ratargul-swamp-forest');
+// touristDestination.pop();
+// console.log(touristDestination);
+
+
+// Task 3
+// let books = ['history', 'theology', 'zoology', 'chemistry', 'mathematics'];
+// if(books.includes('javascript')){
+//     console.log('the book exists in the array');
+// } else{
+//     console.log("there's no such book in the array");
+// }
+
+// Task 4
+// let arrayElements = [1, 2, 3, 4, 5, 6];
+// let number = 1;
+// console.log(Array.isArray(arrayElements));
+// console.log(Array.isArray(number));
+
+// Task 5
+// let premierLeagueTeams = ['Chelsea', 'Liverpool', 'Manchester United', 'West Ham City', 'Crystal Palace'];
+// let laLigaTeams = ['Barcalona', 'Real Madrid', 'Atletico Madrid', 'Villarreal', 'Girona'];
+// let combinedTeams = premierLeagueTeams.concat(laLigaTeams);
+
+// console.log(premierLeagueTeams);
+// console.log(laLigaTeams);
+// console.log(combinedTeams);
+
+
