daf9b04051d8466466e1e5ef44524b9df6851994
YourUsername <mdmehedihasanrafy67865@gmail.com>
2024-09-10 05:45:13 +0600

    loop

---
+++ Programming_Hero/loop-tasks.js
@@ -0,0 +1,34 @@
+// 1. commitment
+// for (let i = 1; i <= 60; i++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+// }
+// let x = 1;
+// while (x <= 60){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+//     x++;
+// }
+
+
+// 2. odd-even
+
+// for (let i = 61; i <= 100; i+=2){
+//     console.log(i);
+// }
+// for (let i = 60; i <= 100; i+=2){
+//     console.log(i);
+// }
+
+// let i = 61;
+// while (i <= 100){
+//     if (i % 2 === 1){
+//         console.log(i);
+//     }
+//     i++;
+// }
+// let i = 61;
+// while (i <= 100){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
---
+++ Programming_Hero/loop.js
@@ -0,0 +1,106 @@
+const numbers = [1, 2, 9, 3, 4, 5];
+// console.log(numbers);
+
+// for of loop
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+//-------------------- while loop-------------------//
+
+// let num = 0;
+// while(num < 5){
+//     console.log(num);
+//     num++;
+// }
+
+// 1 to 10 print
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+// sum of 1 to 10
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     sum += num;
+//     num++;
+// }
+// console.log('sum: ', sum);
+
+// even numbers
+// let num = 1;
+// while(num <= 10){
+//     if(num % 2 == 0)
+//         console.log(num);
+//     num++;
+// }
+
+// decremental 
+// let i = 20;
+// while(i > 10){
+//     console.log(i);
+//     i--;
+// }
+
+// ---------------------- for loop --------------------//
+// for(let num = 0; num < 5; num++){
+//     console.log(num);
+// }
+
+// odd 
+// for(let i = 1; i < 20; i += 2){
+//     console.log(i);
+// }
+
+// decremental 
+// for(let i = 10; i > 0; i--){
+//     console.log(i);
+// }
+
+// divisible by 3 or 5
+// for (let i = 1; i <= 30; i++){
+//     if(i % 3 == 0 || i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+// divisible by 3 and 5
+// for (let i = 1; i <= 30; i++){
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// ----------------- Break & Continue ------------------ //
+
+// Break -- breaks out of loop
+// for (let i = 1; i <= 15; i++){
+//     console.log(i);
+//     if (i >= 10){
+//         break;
+//     }
+// }
+
+// Continue -- skips that iteration
+// for (let i = 1; i <= 15; i++){
+//     if (i === 10){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// ------------------ Do...While Loop ------------------- //
+
+// let n = 1;
+// do{
+//     console.log(n);
+//     n++;
+// }while (n <= 5);
+
+
+
