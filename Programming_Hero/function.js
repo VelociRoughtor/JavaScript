00e91c7040cdf1c0e3f2da6c59dd71b2d2e3b383
YourUsername <mdmehedihasanrafy67865@gmail.com>
2024-09-11 21:17:53 +0600

    function

---
+++ Programming_Hero/function.js
@@ -0,0 +1,19 @@
+// const number =  45;
+// function brush(){
+//     console.log('pick up the brush');
+//     console.log('put toothpaste on it');
+//     console.log('brush your teeth');
+// }
+// brush();
+
+// parameter and argument
+// function square (number){
+//     console.log(number*number);
+// }
+// square(3);
+
+// function add (num1, num2){
+//     const sum = num1 + num2;
+//     console.log(sum);
+// }
+// add(3, 5);
